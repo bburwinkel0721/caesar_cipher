bfdacab6b65db33cf6c8d4807fde19bf0eadad50
bburwinkel <burwinkelben1@gmail.com>
2024-06-10 17:10:52 -0400

    imports and list of alphabet

---
+++ main.py
@@ -0,0 +1,8 @@
+# import art for terminal
+from art import logo
+
+# list for the alphabet
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
